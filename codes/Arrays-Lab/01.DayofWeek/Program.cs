c9079f523ae601792f9980697ab026ef34857959
DSMalamov <dsmalamov@gmail.com>
2022-09-28 22:29:12 +0300

    problems solved

---
+++ codes/Arrays-Lab/01.DayofWeek/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01.DayofWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+
+            string[] daysOfWeek = new string[7];
+            daysOfWeek[0] = "Monday";
+            daysOfWeek[1] = "Tuesday";
+            daysOfWeek[2] = "Wednesday";
+            daysOfWeek[3] = "Thursday";
+            daysOfWeek[4] = "Friday";
+            daysOfWeek[5] = "Saturday";
+            daysOfWeek[6] = "Sunday";
+
+            if (day > 7 || day < 1)
+            {
+                Console.WriteLine("Invalid day!");
+            }
+            else
+            {
+                Console.WriteLine(daysOfWeek[day - 1]);
+            }
+        }
+    }
+}
---
+++ codes/Arrays-Lab/02.PrintNumbersinReverseOrder/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.PrintNumbersinReverseOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+            
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+            
+
+        }
+    }
+}
