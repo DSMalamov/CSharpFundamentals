2250d87787b90ce11f821319af160ac9bd282222
DSMalamov <dsmalamov@gmail.com>
2022-10-08 22:57:37 +0300

    problems solved

---
+++ codes/Methods-Lab/02.Grades/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _02.Grades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            gradeDefinition(grade);
+
+        }
+        static void gradeDefinition (double grade)
+        {
+            if (grade >= 2.00 && grade < 3)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3.00 && grade < 3.50)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.50 && grade < 4.50)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade < 5.50) 
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade >= 5.50 && grade < 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+            
+        
+        }
+    }
+}
---
+++ codes/Methods-Lab/04.PrintingTriangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04.PrintingTriangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            Upper(num);
+            Lower(num);
+
+            static void Upper (int num)
+            {
+                for (int i = 1; i <= num; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            static void Lower (int num)
+            {
+                for (int i = num - 1; i >= 1; i--)
+                {
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
