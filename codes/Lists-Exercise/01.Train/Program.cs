4144d6a9381159882c1d3234909aad2506e1af3e
DSMalamov <dsmalamov@gmail.com>
2022-10-15 23:07:26 +0300

    problems solved

---
+++ codes/Lists-Exercise/01.Train/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listOfWagons = Console.ReadLine()
+                .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            string command = "";
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArg = command
+                    .Split(' ');
+
+                if (cmdArg[0] == "Add")
+                {
+                    int newWagon = int.Parse(cmdArg[1]);
+                    listOfWagons.Add(newWagon);
+                }
+                else
+                {
+                    int passangersToFit = int.Parse(cmdArg[0]);
+
+                    for (int i = 0; i < listOfWagons.Count; i++)
+                    {
+                        int currWagon = listOfWagons[i];
+                        if (currWagon + passangersToFit <= capacity)
+                        {
+                            listOfWagons[i] += passangersToFit;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", listOfWagons));
+        }
+    }
+}
---
+++ codes/Lists-Exercise/02.ChangeList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = "";
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] cmdArg = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (cmdArg[0] == "Delete")
+                {
+                    int element = int.Parse(cmdArg[1]);
+                    input.RemoveAll(x => x == element);
+                }
+                else if (cmdArg[0] == "Insert")
+                {
+                    int elementToInsert = int.Parse(cmdArg[1]);
+                    int position = int.Parse(cmdArg[2]);
+
+                    input.Insert(position, elementToInsert);
+
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", input));
+        }
+    }
+}
