0684f5dd428b7f0c42824560494bdd6ce5be766b
DSMalamov <dsmalamov@gmail.com>
2022-11-18 21:09:16 +0200

    Problems Solved

---
+++ codes/RegularExpressions-Lab/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string patern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(patern);
+
+            MatchCollection collection = regex.Matches(input);
+
+            Console.WriteLine(String.Join(", ", collection));
+
+        }
+    }
+}
