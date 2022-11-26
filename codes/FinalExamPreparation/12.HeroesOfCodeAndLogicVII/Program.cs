fca6f31d5a1b57234b27bb3e2ffd7b5f383c742a
DSMalamov <dsmalamov@gmail.com>
2022-11-27 00:49:18 +0200

    Problems solved

---
+++ codes/FinalExamPreparation/12.HeroesOfCodeAndLogicVII/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _12.HeroesOfCodeAndLogicVII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<int>> heroes = new Dictionary<string, List<int>>();
+            //List contains at 0 hp and at 1 mp
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] heroArg = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string heroName = heroArg[0];
+                int healthPoints = int.Parse(heroArg[1]);
+                int manaPoints = int.Parse(heroArg[2]);
+
+                heroes[heroName] = new List<int> { healthPoints, manaPoints };
+
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] cmdArg = command
+                    .Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+                string cmdInfo = cmdArg[0];
+                string heroName = cmdArg[1];
+
+                if (cmdInfo == "CastSpell")
+                {
+                    int mandNeeded = int.Parse(cmdArg[2]);
+                    string spell = cmdArg[3];
+
+                    if (heroes[heroName][1] >= mandNeeded)
+                    {
+                        heroes[heroName][1] -= mandNeeded;
+                        Console.WriteLine($"{heroName} has successfully cast {spell} and now has {heroes[heroName][1]} MP!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{heroName} does not have enough MP to cast {spell}!");
+                    }
+                }
+                else if (cmdInfo == "TakeDamage")
+                {
+                    int damage = int.Parse(cmdArg[2]);
+                    string attacker = cmdArg[3];
+
+                    heroes[heroName][0] -= damage;
+
+                    if (heroes[heroName][0] <= 0)
+                    {
+                        heroes.Remove(heroName);
+                        Console.WriteLine($"{heroName} has been killed by {attacker}!");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{heroName} was hit for {damage} HP by {attacker} and now has {heroes[heroName][0]} HP left!");
+                    }
+                }
+                else if (cmdInfo == "Recharge")
+                {
+                    int amount = int.Parse(cmdArg[2]);
+
+                    if (heroes[heroName][1] + amount > 200)
+                    {
+                        amount = 200 - heroes[heroName][1];
+                    }
+
+                    heroes[heroName][1] += amount;
+
+                    Console.WriteLine($"{heroName} recharged for {amount} MP!");
+                }
+                else if (cmdInfo == "Heal")
+                {
+                    int amount = int.Parse(cmdArg[2]);
+
+                    if (heroes[heroName][0] + amount > 100)
+                    {
+                        amount = 100 - heroes[heroName][0];
+                    }
+
+                    heroes[heroName][0] += amount;
+
+                    Console.WriteLine($"{heroName} healed for {amount} HP!");
+                }
+            }
+
+            foreach (var kvp in heroes)
+            {
+                Console.WriteLine(kvp.Key);
+                Console.WriteLine($"  HP: {heroes[kvp.Key][0]}");
+                Console.WriteLine($"  MP: {heroes[kvp.Key][1]}");
+            }
+        }
+    }
+}
---
+++ codes/FinalExamPreparation/15.P!rates/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _15.P_rates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command1;
+            Dictionary<string, List<int>> dic = new Dictionary<string, List<int>>();
+            //The list at index 0 = population and at index 1 = gold
+            
+            while ((command1 = Console.ReadLine()) != "Sail")
+            {
+                string[] cmdArg = command1
+                    .Split("||", StringSplitOptions.RemoveEmptyEntries);
+
+                string city = cmdArg[0];
+                int population = int.Parse(cmdArg[1]);
+                int gold = int.Parse(cmdArg[2]);
+
+                if (!dic.ContainsKey(city))
+                {
+                    dic[city] = new List<int> { population, gold };
+                }
+                else
+                {
+                    dic[city][0] += population;
+                    dic[city][1] += gold;
+                }
+                
+            }
+
+            string command2;
+
+            while ((command2 = Console.ReadLine()) != "End")
+            {
+                string[] cmdArg = command2
+                    .Split("=>", StringSplitOptions.RemoveEmptyEntries);
+
+                string cmdInfo = cmdArg[0];
+                string city = cmdArg[1];
+
+                if (cmdInfo == "Plunder")
+                {
+                    int population = int.Parse(cmdArg[2]);
+                    int gold = int.Parse(cmdArg[3]);
+
+                    dic[city][0] -= population;
+                    dic[city][1] -= gold;
+                    Console.WriteLine($"{city} plundered! {gold} gold stolen, {population} citizens killed.");
+
+                    if (dic[city][0] <= 0 || dic[city][1] <= 0)
+                    {
+                        Console.WriteLine($"{city} has been wiped off the map!");
+                        dic.Remove(city);
+                    }
+                }
+                else if (cmdInfo == "Prosper")
+                {
+                    int gold = int.Parse(cmdArg[2]);
+
+                    if (gold < 0)
+                    {
+                        Console.WriteLine($"Gold added cannot be a negative number!");
+                        continue;
+                    }
+
+                    dic[city][1] += gold;
+
+                    Console.WriteLine($"{gold} gold added to the city treasury. {city} now has {dic[city][1]} gold.");
+
+                }
+            }
+
+            if (dic.Count > 0)
+            {
+                Console.WriteLine($"Ahoy, Captain! There are {dic.Count} wealthy settlements to go to:");
+                foreach (var item in dic)
+                {
+                    Console.WriteLine($"{item.Key} -> Population: {dic[item.Key][0]} citizens, Gold: {dic[item.Key][1]} kg");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Ahoy, Captain! All targets have been plundered and destroyed!");
+            }
+        }
+    }
+    
+    
+
+
+    
+    
+}
