d38e3eb0455a48b5f165992d55794894a195e8fd
DSMalamov <dsmalamov@gmail.com>
2022-11-26 23:23:00 +0200

    Problems Solved

---
+++ codes/FinalExamPreparation/13.ActivationKeys/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _13.ActivationKeys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Generate")
+            {
+                string[] cmdArg = command
+                    .Split(">>>", StringSplitOptions.RemoveEmptyEntries);
+
+                string commandInfo = cmdArg[0];
+
+                if (commandInfo == "Contains")
+                {
+                    string substring = cmdArg[1];
+
+                    if (input.Contains(substring))
+                    {
+                        Console.WriteLine($"{input} contains {substring}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Substring not found!");
+                    }
+                }
+                else if (commandInfo == "Flip")
+                {
+                    string letterCase = cmdArg[1];
+                    int startIndex = int.Parse(cmdArg[2]);
+                    int endIndex = int.Parse(cmdArg[3]);
+
+                    if (letterCase == "Upper")
+                    {
+                        string substring = input.Substring(startIndex, endIndex - startIndex).ToUpper();
+                        input = input.Remove(startIndex, endIndex - startIndex);
+                        input = input.Insert(startIndex, substring);
+
+                    }
+                    else if (letterCase == "Lower")
+                    {
+                        string substring = input.Substring(startIndex, endIndex - startIndex).ToLower();
+                        input = input.Remove(startIndex, endIndex - startIndex);
+                        input = input.Insert(startIndex, substring);
+                    }
+
+                    Console.WriteLine(input);
+                    
+                }
+                else if (commandInfo == "Slice")
+                {
+                    int startIndex = int.Parse(cmdArg[1]);
+                    int endIndex = int.Parse(cmdArg[2]);
+
+                    input= input.Remove(startIndex, endIndex - startIndex);
+
+                    Console.WriteLine(input);
+                }
+            }
+
+            Console.WriteLine($"Your activation key is: {input}");
+        }
+    }
+}
---
+++ codes/FinalExamPreparation/14.EmojiDetector/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _14.EmojiDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            BigInteger coolThreshold = 1;
+            coolThreshold = CoolThreshold(input, coolThreshold);
+            Console.WriteLine($"Cool threshold: {coolThreshold}");
+
+            string pattern = @"((\:\:)|(\*\*))(?<emoji>[A-Z][a-z]{2,})(\1)";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection match = regex.Matches(input);
+            Console.WriteLine($"{match.Count} emojis found in the text. The cool ones are:");
+
+            foreach (Match item in match)
+            {
+                string emoji = item.ToString();
+                string emojiValue = item.Groups["emoji"].Value;
+                BigInteger coolness = 0;
+
+                foreach (var ch in emojiValue)
+                {
+                    coolness += (int)ch;
+
+                }
+
+                if (coolness >= coolThreshold)
+                {
+                    Console.WriteLine(emoji );
+                }
+            }
+
+        }
+
+        private static BigInteger CoolThreshold(string input, BigInteger coolThreshold)
+        {
+            foreach (char ch in input)
+            {
+                if (char.IsDigit(ch))
+                {
+                    coolThreshold *= (int)ch - 48;
+                }
+            }
+
+            return coolThreshold;
+        }
+    }
+}
