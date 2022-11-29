9f0ea75c20aeb34822f08d3afdf25aea6de17d69
DSMalamov <dsmalamov@gmail.com>
2022-11-29 22:43:25 +0200

    Problems Solved

---
+++ codes/FinalExamPreparation/05.DestinationMapper/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _05.DestinationMapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(\=|\/)?(?<dest>[A-Z][A-Za-z]{2,})(\1)";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            List<string> desti = new List<string>();
+
+            foreach (Match item in matches)
+            {
+                string currDest = item.Groups["dest"].Value;
+                desti.Add(currDest);
+            }
+
+            Console.WriteLine($"Destinations: {string.Join(", ", desti)}");
+
+            int travelPoints = 0;
+
+            foreach (Match mch in matches)
+            {
+                string currMch = mch.Groups["dest"].Value;
+                travelPoints += currMch.Length;
+            }
+
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
---
+++ codes/FinalExamPreparation/08.MirrorWords/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _08.MirrorWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(\@|\#){1}(?<word>[A-Za-z]{3,}(\1){2}[A-Za-z]{3,})(\1)";
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            if (matches.Count > 0)
+            {
+                Console.WriteLine($"{matches.Count} word pairs found!");
+            }
+            else
+            {
+                Console.WriteLine("No word pairs found!");
+            }
+
+            List<string> mirrorPairs = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                string currMatch = match.Groups["word"].Value;
+                string word1 = currMatch.Substring(0, currMatch.Length / 2  - 1);
+                string word2 = currMatch.Substring(currMatch.Length / 2 + 1, word1.Length);
+                string reversed = string.Empty;
+
+                for (int i = word2.Length - 1; i >= 0; i--)
+                {
+                    reversed += word2[i];
+                }
+                
+
+                string[] currPair = new string[] { word1, word2 };
+
+                if (word1 == reversed)
+                {
+                    mirrorPairs.Add(string.Join(" <=> ", currPair));
+                }
+                
+            }
+
+            if (mirrorPairs.Count == 0)
+            {
+                Console.WriteLine("No mirror words!");
+            }
+            else
+            {
+                Console.WriteLine("The mirror words are:");
+                Console.WriteLine(String.Join(", ", mirrorPairs));
+            }
+            
+        }
+    }
+}
