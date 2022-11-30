aa903449a3b3a62c7aed50f779162dfadae8d75f
DSMalamov <dsmalamov@gmail.com>
2022-11-30 19:34:56 +0200

    Problems Solved

---
+++ codes/FinalExamPreparation/09.NeedForSpeedIII/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09.NeedForSpeedIII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int[]> cars = new Dictionary<string, int[]>();
+
+            //in the int array first index is mileage the second is fuel.
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] currCar = Console.ReadLine()
+                    .Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+                string carName = currCar[0];
+                int mileage = int.Parse(currCar[1]);
+                int fuel = int.Parse(currCar[2]);
+
+                cars[carName] = new int[] {mileage, fuel};
+            }
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Stop")
+            {
+                string[] cmdArg = command
+                    .Split(" : ", StringSplitOptions.RemoveEmptyEntries);
+
+                string cmdInfo = cmdArg[0];
+                string carName = cmdArg[1];
+
+                if (cmdInfo == "Drive")
+                {
+                    int distance = int.Parse(cmdArg[2]);
+                    int fuel = int.Parse(cmdArg[3]);
+
+                    if (cars[carName][1] < fuel)
+                    {
+                        Console.WriteLine("Not enough fuel to make that ride");
+                    }
+                    else 
+                    {
+                        cars[carName][1] -= fuel;
+                        cars[carName][0] += distance;
+
+                        Console.WriteLine($"{carName} driven for {distance} kilometers. {fuel} liters of fuel consumed.");
+
+                        if (cars[carName][0] >= 100000)
+                        {
+                            Console.WriteLine($"Time to sell the {carName}!");
+                            cars.Remove(carName);
+                        }
+                    }
+                }
+                else if (cmdInfo == "Refuel")
+                {
+                    int fuel = int.Parse(cmdArg[2]);
+
+                    if (cars[carName][1] + fuel > 75)
+                    {
+                        fuel = 75 - cars[carName][1];
+                    }
+
+                    cars[carName][1] += fuel;
+
+                    Console.WriteLine($"{carName} refueled with {fuel} liters");
+                }
+                else if (cmdInfo == "Revert")
+                {
+                    int kilometers = int.Parse(cmdArg[2]);
+
+                    if (cars[carName][0] - kilometers < 10000)
+                    {
+                        cars[carName][0] = 10000;  
+                    }
+                    else
+                    {
+                        cars[carName][0] -= kilometers;
+                        Console.WriteLine($"{carName} mileage decreased by {kilometers} kilometers");
+                    }
+                }
+            }
+
+            foreach (var kvp in cars)
+            {
+                Console.WriteLine($"{kvp.Key} -> Mileage: {kvp.Value[0]} kms, Fuel in the tank: {kvp.Value[1]} lt.");
+            }
+        }
+    }
+}
---
+++ codes/FinalExamPreparation/10.PasswordReset/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Text;
+
+namespace _10.PasswordReset
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+
+            string command;
+
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                string[] cmdArg = command
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string cmdInfo = cmdArg[0];
+
+                if (cmdInfo == "TakeOdd")
+                {
+                    string rawPass = string.Empty;
+                    for (int i = 1; i < input.Length; i += 2)
+                    {
+
+                         rawPass += input[i];
+
+                    }
+
+                    input = rawPass;
+                    Console.WriteLine(input);
+                }
+                else if (cmdInfo == "Cut")
+                {
+                    int index = int.Parse(cmdArg[1]);
+                    int length = int.Parse(cmdArg[2]);
+
+                    string substring = input.Substring(index, length);
+                    input = input.Remove(input.IndexOf(substring), substring.Length);
+                    
+                    Console.WriteLine(input);
+
+                }
+                else if (cmdInfo == "Substitute")
+                {
+                    string substring = cmdArg[1];
+                    string substitute = cmdArg[2];
+                    
+                    if (input.Contains(substring))
+                    {
+                        input = input.Replace(substring, substitute);
+                        Console.WriteLine(input);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+                    
+                }
+            }
+
+            Console.WriteLine($"Your password is: {input}");
+        }
+    }
+}
